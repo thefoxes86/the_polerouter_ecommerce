daa07588060df84e0a8700783579e807ab47e762
thefoxes86 <n.volpi@aimconsulting.it>
2021-05-18 18:39:01 +0200

    First Commit

---
+++ src/components/icons/index.js
@@ -0,0 +1,6 @@
+export { default as ArrowDown } from './ArrowDown'
+export { default as Cross } from './Cross'
+export { default as Facebook } from './Facebook'
+export { default as Instagram } from './Instagram'
+export { default as Twitter } from './Twitter'
+export { default as Youtube } from './Youtube'
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  // @see https://tailwindcss.com/docs/upcoming-changes
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    './src/components/**/*.js',
+    './pages/**/*.js'],
+  theme: {
+    extend: {
+      height: {
+        'almost-screen': 'calc(-16rem + 100vh)',
+        '308px': '19.25rem',
+      },
+      width: {
+        '308px': '19.25rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require( 'tailwindcss' ),
+    require( 'precss' ),
+    require( 'autoprefixer' )
+  ]
+}
